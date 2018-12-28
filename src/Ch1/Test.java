597e7e2ea8c29dc475e1faea7e22b481d6e60524
zhiguo.zheng <zhengzhiguo7@gmail.com>
2018-12-28 21:48:02 +0800

    新机Test

---
+++ src/Ch1/Test.java
@@ -0,0 +1,14 @@
+package Ch1;
+
+/**
+ * Description: MSI Test
+ *
+ * @author: 郑智国 (zhiguo.zheng@ucarinc.com)
+ * @time: 2018/12/28 21:46
+ * @version: V1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}

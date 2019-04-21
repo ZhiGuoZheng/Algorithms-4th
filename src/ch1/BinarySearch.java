18fbe33c786b7690d02f17fb4e109c04eefada34
zhiguo.zheng <zhengzhiguo7@gmail.com>
2019-04-21 10:49:38 +0800

    二分查找 增加递归调用方法

--- src/ch1/BinarySearch.java
+++ src/ch1/BinarySearch.java
@@ -15,6 +15,10 @@ import java.util.Arrays;
 public class BinarySearch {
 
     public static int rank(int key, int[] a) {
+
+        // 递归实现
+//        return rank(key, a, 0, a.length - 1);
+
         int lo = 0;
         int hi = a.length - 1;
 
@@ -32,6 +36,30 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 递归实现 二分查找
+     * @param key 查找元素
+     * @param a 数组
+     * @param lo 低位索引
+     * @param hi 高位索引
+     * @return -1 或者索引位置
+     */
+    private static int rank(int key, int[] a, int lo, int hi) {
+        // 如果 key 存在于 a 中，它的索引不会小于 lo 且不会大于 hi
+        if (lo > hi) {
+            return -1;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        if (key < a[mid]) {
+            return rank(key, a, lo, mid - 1);
+        } else if (key > a[mid]) {
+            return rank(key, a, mid + 1, hi);
+        } else {
+            return mid;
+        }
+    }
+
     public static void main(String[] args) {
 
         // 指定文本文件:第一个数字为 key, 后面的为待查找的数据集
@@ -39,6 +67,7 @@ public class BinarySearch {
         Arrays.sort(whitelist);
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
+            // 不存在的话将 key 打印出来
             if (rank(key, whitelist) == -1) {
                 StdOut.println(key);
             }
